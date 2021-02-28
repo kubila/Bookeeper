aa8fc4a57dcbc1458f0f203e722b509801814e6c
kubila <dolaskyasin@gmail.com>
2021-02-28 23:28:52 +0300

    Create Interfaces

---
+++ Booky/Contracts/IBookDetailRepository.cs
@@ -0,0 +1,13 @@
+﻿using Booky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Booky.Contracts
+{
+    public interface IBookDetailRepository : IRepositoryBase<BookDetail>
+    {
+
+    }
+}

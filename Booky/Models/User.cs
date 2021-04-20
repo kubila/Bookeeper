10fde3fb592a11afa1b4b1f6266876c9111b4e49
kubila <dolaskyasin@gmail.com>
2021-04-20 23:47:10 +0300

    Create User and BookViewModel

---
+++ Booky/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Booky.Models
+{
+    public class User : IdentityUser
+    {
+
+        [StringLength(50, ErrorMessage = "First Name must be minium 5 characters and maximum 50 characters."), MinLength(5)]
+        [Required]
+        public string FirstName { get; set; }
+
+        [StringLength(50, ErrorMessage = "Last Name must be minium 5 characters and maximum 50 characters."), MinLength(5)]        
+        [Required]
+        public string LastName { get; set; }
+
+        [NotMapped]
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} + {LastName}";
+            }
+        }
+    }
+}
---
+++ Booky/ViewModels/BookViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Booky.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Booky.ViewModels
+{
+    public class BookViewModel
+    {
+        public int Book_Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(15)]
+        public string ISBN { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        
+        public int Category_Id { get; set; } 
+        public Category Category { get; set; }
+
+        
+        public int BookDetail_Id { get; set; }
+        public BookDetail BookDetail { get; set; }
+
+        
+        public int Publisher_Id { get; set; }
+        public Publisher Publisher { get; set; }
+
+        public ICollection<BookAuthor> BookAuthors { get; set; }
+
+        public ICollection<BookGenre> BookGenres { get; set; }
+    }
+}
